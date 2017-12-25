137ef82a62d8303b5e168ad2385a8e1f071f029e
MissJack <rightjson@gmail.com>
2017-12-25 16:13:42 +0800

    1.0.0

---
+++ NetWorkSwitchTool/NetWorkSwitchTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetWorkSwitchTool
+//
+//  Created by Jack on 2017/12/25.
+//  Copyright © 2017年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
